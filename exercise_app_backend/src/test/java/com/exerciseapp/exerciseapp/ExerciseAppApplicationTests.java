26f3994fa23625295e08f90b05172d3bdef3819a
Albert Hughes <albieh@me.com>
2023-12-19 13:50:04 +0000

    initial commit

---
+++ exercise_app_backend/src/test/java/com/exerciseapp/exerciseapp/ExerciseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exerciseapp.exerciseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
