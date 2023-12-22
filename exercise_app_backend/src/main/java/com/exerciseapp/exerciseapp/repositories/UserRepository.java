bd45e70d1a5d3de6df9995c452fedee85d972cd1
Albert Hughes <albieh@me.com>
2023-12-22 21:32:43 +0000

    added findbyemail in userRepository

--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/repositories/UserRepository.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.exerciseapp.exerciseapp.repositories;
 import com.exerciseapp.exerciseapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 
 
 }
