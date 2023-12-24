14cbde7d3cf31a1ea98edde16debb8eba3ff2581
Albert Hughes <albieh@me.com>
2023-12-24 16:30:34 +0000

    completed authenticate and register inside of authenticationSerivce

---
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/auth/AuthenticationService.java
@@ -0,0 +1,61 @@
+package com.exerciseapp.exerciseapp.auth;
+
+import com.exerciseapp.exerciseapp.config.JwtService;
+import com.exerciseapp.exerciseapp.models.Role;
+import com.exerciseapp.exerciseapp.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository repository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+        var user = User.UserBuilder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+            repository.save(user);
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder() // need to double check this. 1:53
+                    .token()
+                    .build();
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(); // may need to throw the correct exception and handle the correct exceptions later.
+        return AuthenticationResponse.builder() // need to double check this. 1:53
+                .token()
+                .build();
+    }
+}
