03596160e02c557ecbaafde86168ef853b413b03
Albert Hughes <albieh@me.com>
2023-12-24 16:04:40 +0000

    added authenticationManager

--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/config/ApplicationConfig.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/config/ApplicationConfig.java
@@ -3,8 +3,10 @@ package com.exerciseapp.exerciseapp.config;
 import com.exerciseapp.exerciseapp.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,8 +37,15 @@ public class ApplicationConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 
 }
