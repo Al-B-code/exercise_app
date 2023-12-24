a9d2244ca33a71684b8120849378a97bb1a5ba80
Albert Hughes <albieh@me.com>
2023-12-24 16:47:08 +0000

    added lombok to plugins and maven dependencies, refactored authenticationResponse, and request

--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/auth/AuthenticationRequest.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/auth/AuthenticationRequest.java
@@ -1,5 +1,15 @@
 package com.exerciseapp.exerciseapp.auth;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationRequest {
 
 
@@ -8,27 +18,4 @@ public class AuthenticationRequest {
     private String password;
 
 
-    public AuthenticationRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public AuthenticationRequest() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/auth/AuthenticationResponse.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/auth/AuthenticationResponse.java
@@ -1,22 +1,18 @@
 package com.exerciseapp.exerciseapp.auth;
 
 // might need to add lombok to dependencies
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationResponse {
 
     private String token;
 
-    public AuthenticationResponse(String token) {
-        this.token = token;
-    }
-
-    public AuthenticationResponse() {
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
