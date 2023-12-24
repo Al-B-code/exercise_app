89e1e419928729d1fecd79e6c490f683b7a01640
Albert Hughes <albieh@me.com>
2023-12-24 15:58:06 +0000

    Created outline for SecurityFilterChain in SecurityConfig

--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/config/SecurityConfig.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/config/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.exerciseapp.exerciseapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final  AuthenticationProvider authenticationProvider;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http // this way of configuring the securityFilterChain is deprecated and will need to be updated.
+            .csrf()
+            .disable()
+            .authorizeHttpRequests()// this allows us to whitelist the specified urls in the requestmatchers to be visible by default without a jwt token. for example the login page should be visible to all.
+            .requestMatchers("")
+            .permitAll()
+            .anyRequest()// Any other request is required to be authenticated.
+            .authenticated()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // makes a new session for each request.
+            .and()
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // This allows us to execute jwtAuthFilter prior to the usernamePasswordAuthenticationFilter
+
+        return http.build();
+
+    }
+
 }
