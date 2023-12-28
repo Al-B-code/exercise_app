4fb2cfdb4dd6b9821a9875ac38eefeddeae343fc
Albert Hughes <albieh@me.com>
2023-12-28 15:54:07 +0000

    added comment with todo for how to handle logout component in the userDashboard page

--- exercise_app_frontend/src/Pages/UserDashboard.js
+++ exercise_app_frontend/src/Pages/UserDashboard.js
@@ -7,7 +7,7 @@ const UserDashboard = () => {
 
 
 
-
+    // todo: the login button should eventually be added to a nav bar component instead of being added on it's own to the dashboard.
     return ( 
         <>
         <p>Hello From User DashBoard</p>
