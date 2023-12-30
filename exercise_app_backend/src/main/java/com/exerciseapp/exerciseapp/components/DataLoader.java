c80735e6b12a2240a1b323e1b4e7c1d8b05d64d5
Albert Hughes <albieh@me.com>
2023-12-30 21:49:12 +0000

    finished created
    DataLoader class

--- exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/components/DataLoader.java
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/components/DataLoader.java
@@ -1,19 +1,44 @@
 package com.exerciseapp.exerciseapp.components;
 
+import com.exerciseapp.exerciseapp.models.DailyEntry;
+import com.exerciseapp.exerciseapp.models.Role;
 import com.exerciseapp.exerciseapp.models.User;
+import com.exerciseapp.exerciseapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class DataLoader implements ApplicationRunner {
+
+
+    @Autowired
+    UserRepository userRepository;
 
 
 
     @Override
     public void run (ApplicationArguments args) throws Exception {
 
-        User user = new User()
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        List<DailyEntry> DailyEntries = new ArrayList<>();
+
+        User user = new User(
+                "firstNameTest",
+                "lastNameTest",
+                "test@email.com",
+                passwordEncoder.encode("testPassword"),
+                Role.ADMIN,
+                DailyEntries
+        );
+        userRepository.save(user);
 
 
 
