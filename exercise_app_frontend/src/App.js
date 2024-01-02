edfd63f2ba320a676985ae51265f6fe150e84a1c
Albert Hughes <albieh@me.com>
2024-01-02 21:31:18 +0000

    updated Usercontext custom hook

--- exercise_app_frontend/src/App.js
+++ exercise_app_frontend/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import ExerciseAppContainer from './containers/ExerciseAppContainer';
 import AuthProvider from './Provider/AuthProvider';
 import Routes from './routes';
+import UserContext from './contexts/UserContext';
 
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+
   return (
     <>
+    <UserContext.Provider value={{ user, setUser}}>
     <AuthProvider>
       <Routes />
     </AuthProvider>
+    </UserContext.Provider>
     </>
   );
 }
