f26ad0b2f56ed0a16c6340200e173e3e9f7c6096
Albert Hughes <albieh@me.com>
2024-01-03 00:27:59 +0000

    user details are now shown on login

--- exercise_app_frontend/src/containers/UserDashboardContainer.js
+++ exercise_app_frontend/src/containers/UserDashboardContainer.js
@@ -5,11 +5,17 @@ import { UserContext } from "../contexts/UserContext";
 const UserDashboardContainer = () => {
 
 
-
-    
-
+    const { user } = useContext(UserContext);
 
 
+
+    console.log(user);
+
+
+    useEffect(() => {
+
+
+    }, [])
 
 
 
@@ -17,6 +23,11 @@ const UserDashboardContainer = () => {
     return ( 
         <>
         <DashboardSidebar/>
+        <div>
+            <p>Name: {user.firstname}</p>
+            <p>email: {user.email}</p>
+            <p>Role: {user.role}</p>
+        </div>
         </>
     );
 }
