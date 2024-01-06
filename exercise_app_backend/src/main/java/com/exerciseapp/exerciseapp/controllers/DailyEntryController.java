401d645cc4526057550857b6567b492f5df84861
Albert Hughes <albieh@me.com>
2024-01-06 20:54:22 +0000

    created GoalController and DailyEntryController

---
+++ exercise_app_backend/src/main/java/com/exerciseapp/exerciseapp/controllers/DailyEntryController.java
@@ -0,0 +1,14 @@
+package com.exerciseapp.exerciseapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("daily-entry")
+public class DailyEntryController {
+
+
+
+
+
+}
