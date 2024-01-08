8baed4cfe1af24bbbbb1d24c87fe0d65a9f8687e
Albert Hughes <albieh@me.com>
2024-01-08 19:02:42 +0000

    created fetch request for daily entries in the userDashboardContainer passed as a prop to WeightTrackerTile

--- exercise_app_frontend/src/containers/UserDashboardContainer.js
+++ exercise_app_frontend/src/containers/UserDashboardContainer.js
@@ -1,26 +1,49 @@
 import DashboardSidebar from "../components/UserDashboardComponents/DashboardSiderbar";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import WeightTrackerTile from "../components/UserDashboardComponents/WeightTrackerTile";
 import GoalsTile from "../components/UserDashboardComponents/GoalsTile";
+import { useAuth } from "../Provider/AuthProvider";
 
 const UserDashboardContainer = () => {
 
+const { setToken, token, headers } = useAuth();
 
-
-
-
-
-
-
-
-    // console.log(user); 
+const [dailyEntries, setDailyEntries] = useState();
 
 
     useEffect(() => {
 
+        fetchDailyEntries();
+        // console.log(dailyEntries);
 
-    }, [])
+
+    }, []) // probably should have something in this dependency array?
+
+const fetchDailyEntries = async () => {
+    // Need to paginate the daily entries so that the tile only shows the first seven by default.
+    try {
+        const response = await fetch(`http://localhost:8080/daily-entries`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+            },
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error, Status: ${response.status}`);
+        }
+        const data = await response.json()
+        setDailyEntries(data);
+        console.log("This is the daily entries", data);
+    } catch (error) {
+        console.error("error fetching user: ", error)
+        console.log("Error fetching user: ", error)
+        // setError(`Error fetching data ${error}`)
+    }
+
+}
+
+
 
 
 
@@ -28,7 +51,7 @@ const UserDashboardContainer = () => {
     return ( 
         <>
         <DashboardSidebar/>
-        <WeightTrackerTile/>
+        <WeightTrackerTile dailyEntries={dailyEntries}/>
         <GoalsTile/>
         </>
     );
