a77b502f8a6028a0375ea6af845614057fd24e15
Albert Hughes <albieh@me.com>
2024-02-08 21:05:11 +0000

    fixed unique key error for goals tile

--- exercise_app_frontend/src/components/UserDashboardComponents/GoalsTile.js
+++ exercise_app_frontend/src/components/UserDashboardComponents/GoalsTile.js
@@ -17,15 +17,17 @@ const GoalsTile = ({ goals }) => {
     // think of a better name for the goal-tab
     const generateGoalsComponents = () => {
         return goals.map((goal, index) => (
-            <>
-            <div className="single-goal">
-                <h2 key={goal.id}>{goal.goal}</h2>
+            
+
+            <div className="single-goal" key={goal.id}>
+                <h2>{goal.goal}</h2>
+                {console.log(goal.id)}
                 <p>Start Date: {new Date(goal.startDate).toLocaleDateString()}</p>
                 <p>End Date: {new Date(goal.endDate).toLocaleDateString()}</p>
                 <p>Completion Status: {goal.complete ? "Completed" : "Not Completed"}</p>
                 <hr/>
             </div>
-            </>
+    
         ));
     }
     
@@ -33,7 +35,7 @@ const GoalsTile = ({ goals }) => {
     return (
         <div className="goals-tile-container">
             <div className="goals-wrapper">
-        {goals !== null && goals !== undefined && goals.length > 0 ? generateGoalsComponents() : <p>No goals available</p>}
+                {goals !== null && goals !== undefined && goals.length > 0 ? generateGoalsComponents() : <p>No goals available</p>}
             </div>
         </div>
 
