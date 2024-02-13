fd76b6f177ec631e30be178cdf143e00af68d3a9
Albert Hughes <albieh@me.com>
2024-02-13 21:50:02 +0000

    first hero section nearly completed, media queries to be added

--- exercise_app_frontend/src/Pages/Home.js
+++ exercise_app_frontend/src/Pages/Home.js
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "../styles/HomePage.css"
-import loginLogo from "../assets/icons8-user-light.png"
+import "../styles/HomePage.css";
+import lightLoginLogo from "../assets/icons8-user-light.png";
+import blueLoginLogo from "../assets/icons8-user-blue.png";
+import stars from "../assets/review-stars.svg";
+import heroPhone from "../assets/hero-phone-large.png";
 
 const Home = () => {
 
     const [scrollTopData, setScrollTopData] = useState('');
+    const [scrollLoginLogo, setScrollLoginLogo] = useState(lightLoginLogo);
 
     useEffect(() => {
         changeHeaderOnScroll();
@@ -17,8 +21,10 @@ const Home = () => {
         window.addEventListener('scroll', () => {
             if (window.scrollY < 15) {
                 setScrollTopData('');
+                setScrollLoginLogo(lightLoginLogo)
             } else {
                 setScrollTopData('scrolled');
+                setScrollLoginLogo(blueLoginLogo)
             }
         })
     }
@@ -29,11 +35,30 @@ const Home = () => {
             <div className={`home-page-header ${scrollTopData}`}>
                 <div className="title-login-wrapper">
                     <h1 className="header-title">myexerciseapp</h1>
-                
-                    <NavLink to = "/login" className="header-login-nav-button"><a href="" className="login-logo-a"><img className="login-logo-img" src={loginLogo} alt="login-logo-button"/></a></NavLink>
+                    <NavLink to = "/login" className="header-login-nav-button"><a href="" className="login-logo-a"><img className="login-logo-img" src={scrollLoginLogo} alt="login-logo-button"/></a></NavLink>
                 </div>
             </div>
             <div className="home-page-body">
+                <section className="home-page-body-slice" id="start-today-slice">
+                    <div className="app-advert">
+                        <div className="app-advert-text">
+                            <h1 className="app-advert-h1">
+                                <span className="no1-exercise-app font-light">#1 exercise tracking app</span>
+                                <span className="reach-your-goals font-light">Reach your goals</span> 
+                                <span className="with-myexerciseapp font-light">With MyExerciseApp</span>
+                            </h1>
+                            <p className="advert-tagline font-light">Build healthy habits with the all-in-one food, exercise, and calorie tracker.</p>
+                        </div>
+                        <img className="hero-phone-img" alt="image of a phone displaying the my exercise app" src={heroPhone}></img>
+
+                    </div>
+
+                </section>
+                <section className="home-page-body-slice" id="reviews">
+                    <p>test</p>
+                    <img className="review-stars-svg" src={stars}></img>
+
+                </section>
                 <section className="home-page-body-slice" id="start-today-slice">
                     <p>test</p>
 
