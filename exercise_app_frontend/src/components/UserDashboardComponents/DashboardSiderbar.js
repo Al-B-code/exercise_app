ef6dbc75da12916709ff7ebab5042c83934fd3b0
Albert Hughes <albieh@me.com>
2024-02-27 20:21:35 +0000

    added styling to sidebar navigation

--- exercise_app_frontend/src/components/UserDashboardComponents/DashboardSiderbar.js
+++ exercise_app_frontend/src/components/UserDashboardComponents/DashboardSiderbar.js
@@ -25,24 +25,26 @@ const DashboardSidebar = () => {
         <div className="side-bar">
         <p>Welcome</p>
 
-        <div className="user-details">
-            {user ? <p>name: {user.firstName}</p> : <p>Loading</p>}
-            {user ? <p>email: {user.email}</p> : <p>Loading</p>}
-            {user ? <p>name: {user.role}</p> : <p>Loading</p>}
-        </div>
-      
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Profile</li>
-                <li>Settings</li>
-            </ul>
-        </nav>
-        <button id="daily-entry-modal-button" onClick={() => setIsOpen(true)}>Submit a daily entry</button>
-        {isOpen && <DailyEntryModal setIsOpen={setIsOpen} />}
+            <div className="user-details">
+                {user ? <p>name: {user.firstName}</p> : <p>Loading</p>}
+                {user ? <p>email: {user.email}</p> : <p>Loading</p>}
+                {user ? <p>role: {user.role}</p> : <p>Loading</p>}
+            </div>
+            <button id="daily-entry-modal-button" onClick={() => setIsOpen(true)}>Submit a daily entry</button>
 
-
-        <Logout/>
+        
+            <nav>
+                <ul>
+                    <li><span className="sidebar-nav-list-element">Home</span></li>
+                    <li><span className="sidebar-nav-list-element">Profile</span></li>
+                    <li><span className="sidebar-nav-list-element">Settings</span></li>
+                    <li><span className="sidebar-nav-list-element">Weight</span></li>
+                    <li><span className="sidebar-nav-list-element">Calories</span></li>
+                    <li><span className="sidebar-nav-list-element">Goals</span></li>
+                </ul>
+            </nav>
+            {isOpen && <DailyEntryModal setIsOpen={setIsOpen} />}
+            <Logout/>
 
         </div>
 
